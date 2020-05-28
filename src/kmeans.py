450f701c2a287716a3f2c74ac276544d59de7a81
Sergi Simon Balcells <sergisiba@gmail.com>
2020-05-28 19:04:42 +0200

    removed points

--- src/kmeans.py
+++ src/kmeans.py
@@ -8,15 +8,13 @@ import functools
 import collections
 from math import sin, cos, sqrt, atan2, radians
 
-Point = collections.namedtuple("Point", ["lat", "lon"])
-
 def spheric_distance(point1, point2):
     # approximate radius of earth in km
     R = 6373.0
-    lat1 = radians(point1.lat)
-    lon1 = radians(point1.lon)
-    lat2 = radians(point2.lat)
-    lon2 = radians(point2.lon)
+    lat1 = radians(point1[0])
+    lon1 = radians(point1[1])
+    lat2 = radians(point2[0])
+    lon2 = radians(point2[1])
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
