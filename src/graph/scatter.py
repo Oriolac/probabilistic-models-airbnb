4642c0ead0a1c6b07d18d0300d4d8d8903182daa
Sergi Simon Balcells <sergisiba@gmail.com>
2020-05-28 21:18:19 +0200

    Deleted images

--- src/graph/scatter.py
+++ src/graph/scatter.py
@@ -5,10 +5,10 @@ import numpy as np
 def get_lat_longs():
     pass
 
-def main(file):
-    df = pd.read_csv(file, names=["type", "nb", "reviews", "sat", "acc", "bed", "price", "lat", "lon"])
+def main(filen):
+    df = pd.read_csv(filen, header=0)
     fig, ax = plt.subplots()
-    ax.scatter(df["lat"], df["lon"])
+    ax.scatter(df["latitude"], df["longitude"], c=df["overall_satisfaction"])
     ax.set_xlabel('Latitude')
     ax.set_ylabel('Longitude')
     ax.set_title(f'Representation in a cartesian world')
