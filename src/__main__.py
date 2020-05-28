7f661f5dd96791f081144abd68e826da97b644be
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-28 20:16:24 +0200

    DBSCAN

--- src/__main__.py
+++ src/__main__.py
@@ -2,15 +2,29 @@ import sys
 
 import src.kmeans as km
 import pandas as pd
+import numpy as np
+from sklearn.cluster import DBSCAN
+import math as mt
+import matplotlib.pyplot as plt
+
+
+def plot(db,df):
+    plt.scatter(df['latitude'], df['longitude'], c=db.core_sample_indices_)
+    plt.savefig('algo.png')
+
 
 
 def main(file):
-    df = pd.read_csv(file, names=["type", "nb", "reviews", "sat", "acc", "bed", "price", "lat", "lon"])
-    iter_r = df.iterrows()
-    next(iter_r)
-    rows = [km.Point(x['lat'], x['lon']) for i, x in iter_r]
-    print(km.kclusters(rows, kcentroid=8))
-
+    df = pd.read_csv(file, header=0,
+                     dtype={"latitude": np.float, "longitude": np.float})
+    coords = df[['latitude', 'longitude']]
+    db = DBSCAN(eps=0.19/6371., algorithm='ball_tree', metric='haversine').fit(np.radians(coords))
+    cluster_labels = db.labels_
+    plot(db,coords)
+    num_clusters = len(set(cluster_labels))
+    clusters = pd.Series([coords[cluster_labels == n] for n in range(num_clusters)])
+    print('Number of clusters: {}'.format(num_clusters))
+    print(clusters)
 
 
 if __name__ == '__main__':
