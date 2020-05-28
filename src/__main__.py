b45223d7cf53b3691cca6fe15781fcebf859f932
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-28 20:34:42 +0200

    dbscsad

--- src/__main__.py
+++ src/__main__.py
@@ -9,8 +9,28 @@ import matplotlib.pyplot as plt
 
 
 def plot(db,df):
-    plt.scatter(df['latitude'], df['longitude'], c=db.core_sample_indices_)
-    plt.savefig('algo.png')
+    labels = db.labels_
+    unique_labels = set(labels)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    colors = [plt.cm.Spectral(each)
+              for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+
+        xy = df[class_member_mask & core_samples_mask]
+        plt.plot(xy['latitude'], xy['longitude'], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=14)
+
+        xy = df[class_member_mask & ~core_samples_mask]
+        plt.plot(xy['latitude'], xy['longitude'], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=6)
+
+    plt.savefig('scatter.png')
 
 
 
@@ -18,7 +38,7 @@ def main(file):
     df = pd.read_csv(file, header=0,
                      dtype={"latitude": np.float, "longitude": np.float})
     coords = df[['latitude', 'longitude']]
-    db = DBSCAN(eps=0.19/6371., algorithm='ball_tree', metric='haversine').fit(np.radians(coords))
+    db = DBSCAN(eps=0.18/6371., min_samples=5, algorithm='ball_tree', metric='haversine').fit(np.radians(coords))
     cluster_labels = db.labels_
     plot(db,coords)
     num_clusters = len(set(cluster_labels))
