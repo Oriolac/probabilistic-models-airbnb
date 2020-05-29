88025ff63cd0c796a814f8a670ce1e8f883774b0
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-29 14:51:58 +0200

    Argument categorize func in argparse

--- src/__main__.py
+++ src/__main__.py
@@ -1,67 +1,35 @@
 import sys
+from typing import List
 
-import random
 import pandas as pd
-import numpy as np
-import math as mt
-import matplotlib.pyplot as plt
 import argparse
 
+from src.weka import categorize, parse_weka
 
-def categorize_dataframe_nquartiles(dataframe, column, number_quantiles):
-    dataframe[column] = pd.qcut(dataframe[column],
-                                number_quantiles, labels=False)
+CAT_MAP = {'qcut': pd.qcut,
+           'cut': pd.cut}
 
-def parse_weka(df, relation_name, seed):
-    '''
-        Parses a pandas.DataFrame to weka format, independently
-        from the weka format. All attributes must be categorized
-        before this function.
-        It's a pure function.
-
-        :param df: pandas.DataFrame -> DataFrame to be parsed.
-        :param relation_name: str -> relation name used in weka.
-        Its the first line containing:
-         `@RELATION {relation_name}`
-
-        :return string with weka format-like. With this it's
-        straight-forward to save it in a file.
-    '''
-
-    def sample_arff(prefix, sample):
-        res = prefix + '\n\n@DATA\n'
-        res += '\n'.join(','.join(f"'{str(x)}'" for x in row) 
-                for row in sample.values)
-        return res
-
-    prefix = f"@RELATION {relation_name}\n\n"
-
-    prefix += '\n'.join(f'@ATTRIBUTE {col} ' + '{' +
-            ','.join(f"'{str(x)}'" for x in df[col].unique())
-            + '}' for col in df.columns)
-    train = df.sample(frac=0.75, random_state=seed)
-    test = df.drop(train.index)
-    return sample_arff(prefix, train), sample_arff(prefix, test)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Parses parameters for '
-            'tranforming the csv to an arff file')
-
-    parser.add_argument('inputfile', metavar='input-file', type=str, 
-            help='Input file in csv to be used for parsing')
+                                                 'tranforming the csv to an arff file')
+    parser.add_argument('inputfile', metavar='input-file', type=str,
+                        help='Input file in csv to be used for parsing')
     parser.add_argument('trainfile', metavar='train-file', type=str,
-            help='Train file in arff format to be used at weka.'
-            'Uses about a 75%% of the rows in the input file')
+                        help='Train file in arff format to be used at weka.'
+                             'Uses about a 75%% of the rows in the input file')
     parser.add_argument('testfile', metavar='test-file', type=str,
-            help='Test file in arff format to be used at weka.'
-            'Uses about a 25%% od the rows in the input file, and'
-            'testfile union trainfile = inputfile in rows')
+                        help='Test file in arff format to be used at weka.'
+                             'Uses about a 25%% od the rows in the input file, and'
+                             'testfile union trainfile = inputfile in rows')
     for arg in ['price', 'reviews', 'latitude', 'longitude']:
         parser.add_argument(f'-{arg[:3]}', f'--nquartile-{arg}', dest=f'{arg}q',
-            type=int, default=4, help=f'Number of quartiles for {arg}')
+                            type=int, default=4, help=f'Number of quartiles for {arg}')
     parser.add_argument('-n', '--relation-name', dest='name',
-            type=str, default='airbnb', help='Name of the relation in the file')
+                        type=str, default='airbnb', help='Name of the relation in the file')
     parser.add_argument('-s', '--seed', dest='seed', type=int, default=0o4011,
+                        help='Seed to be used for spliting data')
+    parser.add_argument('-c', '--categorize', dest='categorize', choices=CAT_MAP.keys(), default='qcut',
                         help='Seed to be used for spliting data')
     return parser.parse_args()
 
@@ -69,10 +37,10 @@ def parse_arguments():
 def main():
     args = parse_arguments()
     df = pd.read_csv(args.inputfile, header=0)
-    categorize_dataframe_nquartiles(df, 'price', args.priceq)
-    categorize_dataframe_nquartiles(df, 'reviews', args.reviewsq)
-    categorize_dataframe_nquartiles(df, 'latitude', args.latitudeq)
-    categorize_dataframe_nquartiles(df, 'longitude', args.longitudeq)
+    categorize(df, 'price', args.priceq, CAT_MAP[args.categorize])
+    categorize(df, 'reviews', args.reviewsq, CAT_MAP[args.categorize])
+    categorize(df, 'latitude', args.latitudeq, CAT_MAP[args.categorize])
+    categorize(df, 'longitude', args.longitudeq, CAT_MAP[args.categorize])
 
     train, test = parse_weka(df, args.name, args.seed)
     print(train, file=open(args.trainfile, 'w'))
@@ -81,7 +49,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-    print('exiting')
     sys.exit()
-
-
---
+++ src/weka.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+
+def categorize(dataframe, column, number_quantiles, cat_func):
+    dataframe[column] = cat_func(dataframe[column],
+                                number_quantiles, labels=False)
+
+
+def parse_weka(df, relation_name, seed):
+    '''
+        Parses a pandas.DataFrame to weka format, independently
+        from the weka format. All attributes must be categorized
+        before this function.
+        It's a pure function.
+
+        :param df: pandas.DataFrame -> DataFrame to be parsed.
+        :param relation_name: str -> relation name used in weka.
+        Its the first line containing:
+         `@RELATION {relation_name}`
+
+        :return string with weka format-like. With this it's
+        straight-forward to save it in a file.
+    '''
+
+    def sample_arff(prefix, sample):
+        res = prefix + '\n\n@DATA\n'
+        res += '\n'.join(','.join(f"'{str(x)}'" for x in row)
+                         for row in sample.values)
+        return res
+
+    prefix = f"@RELATION {relation_name}\n\n"
+
+    prefix += '\n'.join(f'@ATTRIBUTE {col} ' + '{' +
+                        ','.join(f"'{str(x)}'" for x in df[col].unique())
+                        + '}' for col in df.columns)
+    train = df.sample(frac=0.75, random_state=seed)
+    test = df.drop(train.index)
+    return sample_arff(prefix, train), sample_arff(prefix, test)
